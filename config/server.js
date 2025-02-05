40d5c912a53b75f29a03e48cd8bcdec158f50b67
kariimellsayed <kariimellsayed@gmail.com>
2025-02-05 23:13:01 +0200

    some update

--- config/server.js
+++ config/server.js
@@ -1,7 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 8000), // استخدم 8000 بدلًا من 1337
+  host: "0.0.0.0", // استقبل أي اتصال خارجي
+  port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  url: env("PUBLIC_URL", ""), // إضافة المتغير الجديد
 });
